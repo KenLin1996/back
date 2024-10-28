d1f7f1d0c49e23d70e445958556ce1daa5e7a470
Ken Lin <jesus0910727994@gmail.com>
2024-10-28 18:27:59 +0800

    import passport

--- routes/externalAuth.js
+++ routes/externalAuth.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { googleAuthLog } from "../controllers/externalAuth.js";
 import * as auth from "../middlewares/auth.js";
+import passport from "passport";
 
 const router = Router();
 
